cc541df86d84ad6abd5f27346e1d02c741e5094f
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-06-22 20:43:12 +0530

    interrupts: Initialize PICs

--- kernel/idt.h
+++ kernel/idt.h
@@ -3,6 +3,25 @@
 
 #include <inttypes.h>
 
+#define PIC1 0x20       /* IO base address for master PIC */
+#define PIC2 0xA0       /* IO base address for slave PIC */
+#define PIC1_COMMAND  PIC1
+#define PIC1_DATA	    (PIC1+1)
+#define PIC2_COMMAND  PIC2
+#define PIC2_DATA	    (PIC2+1)
+
+#define ICW1_ICW4       0x01  /* ICW4 (not) needed */
+#define ICW1_SINGLE	    0x02  /* Single (cascade) mode */
+#define ICW1_INTERVAL4  0x04  /* Call address interval 4 (8) */
+#define ICW1_LEVEL      0x08  /* Level triggered (edge) mode */
+#define ICW1_INIT       0x10  /* Initialization - required! */
+
+#define ICW4_8086	      0x01  /* 8086/88 (MCS-80/85) mode */
+#define ICW4_AUTO       0x02  /* Auto (normal) EOI */
+#define ICW4_BUF_SLAVE  0x08  /* Buffered mode/slave */
+#define ICW4_BUF_MASTER 0x0C  /* Buffered mode/master */
+#define ICW4_SFNM       0x10  /* Special fully nested (not) */
+
 /**
  * The structure of the IDT
  *
@@ -36,5 +55,9 @@ typedef struct _idt_ptr_t {
 } idt_ptr_t;
 
 #define IDT_SIZE 256
+
+void idt_init(void);
+void idt_set_gate(int offset, uint32_t base, uint16_t selector,
+    uint8_t type_attr);
 
 #endif /* end of include guard: IDT_H_7TALJSEW */
---
+++ kernel/util/util.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_H_MPHHJCA7
+#define UTIL_H_MPHHJCA7
+
+extern unsigned int  read_port   (unsigned int port);
+extern void          write_port  (unsigned int port, unsigned int data);
+
+#endif /* end of include guard: UTIL_H_MPHHJCA7 */
