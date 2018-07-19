265f196e11a689dd69080632ef3c519c46651745
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-07-19 15:42:55 +0000

    util: Add integer types definition

---
+++ kernel/util/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H_JLM8TSQD
+#define TYPES_H_JLM8TSQD
+
+/* Define integer types */
+typedef signed char         int8_t;
+typedef unsigned char       uint8_t;
+typedef signed short int    int16_t;
+typedef unsigned short int  uint16_t;
+typedef signed int          int32_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
+typedef long long           int64_t;
+
+#endif /* end of include guard: TYPES_H_JLM8TSQD */
