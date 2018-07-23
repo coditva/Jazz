c34a9b83befca7853d00660420195d359fb89a8f
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-07-23 11:57:54 +0000

    interrupts: Add keyboard map

--- kernel/isr/isr.c
+++ kernel/isr/isr.c
@@ -2,13 +2,22 @@
 #include "isr/isr.h"
 #include "util/video.h"
 #include "util/util.h"
+#include "util/keyboard_map.h"
 
 extern void keyboard_handler_int(void);
 
 void keyboard_handler(void)
 {
-  /* TODO */
-  kputc('a');
+  uint8_t status;
+
+  write_port(0x20, 0x20);   /* ask for status */
+  status = read_port(0x64); /* get status */
+
+  if (status & 0x01) {      /* buffer is not empty */
+    char data = -1;
+    if ((data = read_port(0x60)) < 0) return; /* invalid data */
+    kputc(keyboard_map[data]);
+  }
 }
 
 void isr_set_keyboard(void)
