8c00aec07069db59bbd21d3fff070c1992536ab3
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-07-23 11:57:54 +0000

    interrupts: Add keyboard map

---
+++ kernel/util/keyboard_map.h
@@ -0,0 +1,25 @@
+#ifndef KEYBOARD_MAP_H_8SQJI62I
+#define KEYBOARD_MAP_H_8SQJI62I
+
+char keyboard_map[] = {
+  0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+  '\b' /* backspace */,
+  '\t' /* tab */,
+
+  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
+  '\n' /* enter */,
+  0    /* control key */,
+
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
+
+  0    /* left shift */,
+  '\\',
+  'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+  0    /* right shift */,
+
+  '*',
+
+  /* TODO: Define the rest */
+};
+
+#endif /* end of include guard: KEYBOARD_MAP_H_8SQJI62I */
