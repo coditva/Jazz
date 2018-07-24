05d898d027e6b232525d012c75b695cbea5d9869
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-07-24 16:34:29 +0000

    kernel: Add log messages while initializing

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,13 +5,18 @@ extern void kmain()
 {
   kclear_screen();
 
+  eprintf(ERR_LOG, "\nInitializing IDT... ");
   idt_init();
+  eprintf(ERR_LOG, "OK");
 
+  eprintf(ERR_LOG, "\nInitializing ISRs... ");
   isr_set_keyboard();
   isr_set_double_fault();
   isr_init_keyboard();
+  eprintf(ERR_LOG, "OK");
 
-  kprintf("type here: ");
+  eprintf(ERR_LOG, "\nInitialization complete.\n");
+  kprintf("\n$ ");
 
   while(1)
     ; /* nop */
