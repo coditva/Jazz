c709352b2c7d97fea4db637b105c281feb991397
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-07-24 11:13:49 +0000

    interrupts: Clear direction flag on entry

--- kernel/interrupt/handler.asm
+++ kernel/interrupt/handler.asm
@@ -2,6 +2,9 @@
 
 global keyboard_handler_int
 keyboard_handler_int:
+  pushad
+  cld                       ; clear the direction flag on function entry
   extern keyboard_handler
   call keyboard_handler
-  iretd
+  popad
+  iret
