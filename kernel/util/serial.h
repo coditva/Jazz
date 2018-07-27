0a5371d90ab41ede5a536d205c5257de37ce35aa
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-07-27 16:31:14 +0530

    serialio: Define serialio interface

---
+++ kernel/util/serial.h
@@ -0,0 +1,12 @@
+#ifndef SERIAL_H_FT2N9HWC
+#define SERIAL_H_FT2N9HWC
+
+#define SERIAL_PORT1 0x3f8
+#define SERIAL_PORT2 0x2f8
+#define SERIAL_PORT3 0x3e8
+#define SERIAL_PORT4 0x2e8
+
+void serial_write   (int port, int data);
+int  serial_read    (int port);
+
+#endif /* end of include guard: SERIAL_H_FT2N9HWC */
