879ccd6a491bc9e0d02c58e6c47480263d970089
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-08-07 19:04:26 +0530

    serialio: Add function to log to serial port

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,28 +2,30 @@
 #include "serial/serial.h"
 #include "interrupt/interrupt.h"
 
+#include "debug/debug.h"
+
 extern void kmain()
 {
   kclear_screen();
 
-  eprintf(ERR_LOG, "\nInitializing Serial ports... ");
+  klog(ERR_LOG, "\nInitializing Serial ports... ");
   serial_init(SERIAL_PORT1);
   serial_init(SERIAL_PORT2);
   serial_init(SERIAL_PORT3);
   serial_init(SERIAL_PORT4);
-  eprintf(ERR_LOG, "OK");
+  klog(ERR_LOG, "OK");
 
-  eprintf(ERR_LOG, "\nInitializing IDT... ");
+  klog(ERR_LOG, "\nInitializing IDT... ");
   idt_init();
-  eprintf(ERR_LOG, "OK");
+  klog(ERR_LOG, "OK");
 
-  eprintf(ERR_LOG, "\nInitializing ISRs... ");
+  klog(ERR_LOG, "\nInitializing ISRs... ");
   isr_set_keyboard();
   isr_set_double_fault();
   isr_init_keyboard();
-  eprintf(ERR_LOG, "OK");
+  klog(ERR_LOG, "OK");
 
-  eprintf(ERR_LOG, "\nInitialization complete.\n");
+  klog(ERR_LOG, "\nInitialization complete.\n");
   kprintf("\n$ ");
 
   while(1)
