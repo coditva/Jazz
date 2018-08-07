28c17110890d484df162fa9d5afcd6988dc71ed1
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-08-07 17:33:23 +0530

    serialio: Add functions to read/write to serial ports

--- kernel/util/serial.c
+++ kernel/util/serial.c
@@ -1,9 +1,21 @@
 #include "serial.h"
+#include "util/portio.h"
+
+#define SERIAL_RECEIVED(_PORT)    (read_port(_PORT + 5) & 1)
+#define SERIAL_SEND_EMPTY(_PORT)  (read_port(_PORT + 5) & 1)
 
 void serial_write(int port, int data)
 {
+  while (SERIAL_SEND_EMPTY(port) == 0)
+    ; /* nop */
+
+  write_port(port, data);
 }
 
 int serial_read(int port)
 {
+  while (SERIAL_RECEIVED(port) == 0)
+    ; /* nop */
+
+  return read_port(port);
 }
