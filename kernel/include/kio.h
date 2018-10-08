cd88b9961f3fad4970d8aba3ee38b01d77052450
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-10-08 20:25:06 +0530

    lib: Define sprintf/vsprintf interface correctly
    TODO: The sprintf behaves like vsprintf!

--- kernel/include/kio.h
+++ kernel/include/kio.h
@@ -1,9 +1,14 @@
 #ifndef KIO_H_PI0IE1TW
 #define KIO_H_PI0IE1TW
 
-int     kputc       (const char data);
-int     kprintf     (const char *format, ...);
-void    keprintf    (int error_level, const void *format, ...);
+#include <stdarg.h>
 
+int     kputc       (const char data);
+
+int     kprintf     (const char *format, ...);
+int     keprintf    (int error_level, const void *format, ...);
+
+int     kvprintf    (const char *format, va_list args);
+int     kveprintf   (int error_level, const void *format, va_list args);
 
 #endif /* end of include guard: KIO_H_PI0IE1TW */
