0c5d6a0d7b70f0428ebd8a4a026bbd73712c4bde
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-10-08 20:25:06 +0530

    lib: Define sprintf/vsprintf interface correctly
    TODO: The sprintf behaves like vsprintf!

--- kernel/include/io.h
+++ kernel/include/io.h
@@ -1,10 +1,14 @@
 #ifndef IO_H_F7KRHNJL
 #define IO_H_F7KRHNJL
 
-int vputc      (char *buffer, const char data);
-int vputd      (char *buffer, const int data);
-int vputx      (char *buffer, const int data);
-int vputs      (char *buffer, const char *data);
-int vsprintf   (char *buffer, const char *format, ...);
+#include <stdarg.h>
+
+int sputc      (char *buffer, const char data);
+int sputd      (char *buffer, const int data);
+int sputx      (char *buffer, const int data);
+int sputs      (char *buffer, const char *data);
+int sprintf    (char *buffer, const char *format, ...);
+
+int vsprintf   (char *buffer, const char *format, va_list args);
 
 #endif /* end of include guard: IO_H_F7KRHNJL */
