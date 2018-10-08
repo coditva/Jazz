66068d66d15eff7ad6dff82b59dc567088024733
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-10-08 17:54:33 +0530

    lib: Implement stdarg.h

---
+++ kernel/include/stdarg.h
@@ -0,0 +1,15 @@
+#ifndef STDARG_H_WG3Q0UEA
+#define STDARG_H_WG3Q0UEA
+
+#define va_list char *
+
+#define va_rounded_size(TYPE) \
+  ((sizeof(TYPE) + sizeof(int) - 1) & ~(sizeof(int) - 1))
+
+#define va_start(AP, LAST) \
+  (AP = (va_list)&LAST + va_rounded_size(LAST))
+
+#define va_arg(AP, TYPE) \
+  (*(TYPE *)((AP += va_rounded_size(TYPE)) - va_rounded_size(TYPE)))
+
+#endif /* end of include guard: STDARG_H_WG3Q0UEA */
