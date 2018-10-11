74afaa6ca10b34c324d0e54aa351924371cf20d6
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-10-11 17:06:24 +0530

    mm: Implement page_frame_alloc/free

--- kernel/include/page_frame.h
+++ kernel/include/page_frame.h
@@ -5,8 +5,8 @@
 #include <multiboot.h>
 
 int         page_frame_init     (multiboot_info_t *multiboot_info);
-uint32_t *  page_frame_alloc    (void);
-int         page_frame_free     (uint32_t *page_addr);
+void *      page_frame_alloc    (void);
+void        page_frame_free     (void *page_addr);
 void        page_frame_dump_map (void);
 
 #endif /* end of include guard: PAGE_FRAME_ALLOC_H_S1KP2ELH */
