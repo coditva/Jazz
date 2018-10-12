2818f29c944ed83d03ee1ded445a06ffa4ce2bcb
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-10-12 15:38:47 +0530

    config: Fix readability

--- kernel/io/serial/serial.h
+++ kernel/io/serial/serial.h
@@ -1,10 +1,17 @@
 #ifndef SERIAL_H_FT2N9HWC
 #define SERIAL_H_FT2N9HWC
 
+#include "config.h"
+
 #define SERIAL_PORT1 0x3f8
 #define SERIAL_PORT2 0x2f8
 #define SERIAL_PORT3 0x3e8
 #define SERIAL_PORT4 0x2e8
+
+#ifdef DEBUG_TO_SERIAL
+# include "io/serial/serial.h"
+# define DEBUG_SERIAL_PORT SERIAL_PORT1
+#endif
 
 void serial_init    ();
 void serial_write   (int port, char *data);
