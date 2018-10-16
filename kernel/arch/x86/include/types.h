86fa978571322033e43dc867fe1f17bc3225c93b
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-10-16 13:59:30 +0530

    lib: Use "unsigned long" of the host OS
    This is a temporary change till compiler toolchain is not setup
    properly.

--- kernel/arch/x86/include/types.h
+++ kernel/arch/x86/include/types.h
@@ -3,6 +3,9 @@
 
 #include "arch/generic/include/types.h"
 
+/* TODO: Setup compiler toolchain and remove this */
+#if 0
+
 #ifdef CONFIG_64BIT
 # define BITS_PER_LONG 64
 # define __kernel_ulong_t    __u64
@@ -11,6 +14,14 @@
 # define BITS_PER_LONG 32
 # define __kernel_ulong_t    __u32
 # define __kernel_long_t     __s32
+#endif
+
+#else
+
+# define BITS_PER_LONG 32
+# define __kernel_ulong_t    unsigned long
+# define __kernel_long_t     unsigned long
+
 #endif
 
 #endif /* end of include guard: TYPES_H_6BL3PEFC */
