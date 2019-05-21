3a3e2adc98c24f24e912dc567a6a1a32dfc3fa85
Utkarsh Maheshwari <coditva@gmail.com>
2019-05-22 01:13:20 +0530

    mm: Fix paging_dump_map and paging_unmap_page

--- kernel/mm/paging.c
+++ kernel/mm/paging.c
@@ -1,17 +1,17 @@
+#include <assert.h>
 #include <logger.h>
 #include <mm/page_frame.h>
 #include <mm/paging.h>
 #include <mm/types.h>
-#include <types.h>
-#include <assert.h>
 #include <string.h>
+#include <types.h>
 
 struct page_directory_entry *page_directory = NULL;
 
 extern uintptr_t startkernel;
 extern uintptr_t endkernel;
 
-static inline void * 
+static inline void *
 page_directory_to_address(struct page_directory_entry *page_dir_entry)
 {
   return (void *)((uintptr_t)page_dir_entry->value & 0xfffff000);
@@ -28,6 +28,12 @@ page_directory_set_value(struct page_directory_entry *page_dir_entry,
     uint32_t value)
 {
   page_dir_entry->value = value;
+}
+
+static inline void *
+page_table_to_address(struct page_table_entry *page_tab_entry)
+{
+  return (void *)((uintptr_t)page_tab_entry->value & 0xfffff000);
 }
 
 static inline uint32_t
@@ -102,7 +108,7 @@ int paging_map_page(void *physical_address, void *virtual_address,
     page_directory_set_value(&page_directory[page_dir_index], (uintptr_t)page_table);
     page_directory[page_dir_index].rw = 1;
     page_directory[page_dir_index].present = 1;
-    klog(LOG_DEBUG, "new directory entry: 0x%x\n",
+    klog(LOG_DEBUG, "paging_map_page: new directory entry: 0x%x\n",
         page_directory_get_value(&page_directory[page_dir_index]));
   }
 
@@ -115,7 +121,7 @@ int paging_map_page(void *physical_address, void *virtual_address,
   page_table_set_value(&page_table[page_tab_index],
       (uintptr_t)physical_address | (flags & 0xfff));
   page_table[page_tab_index].present = 1;
-  klog(LOG_DEBUG, "new table entry: 0x%x\n",
+  klog(LOG_DEBUG, "paging_map_page: new table entry: 0x%x\n",
       page_table_get_value(&page_table[page_tab_index]));
 
   return 0;
@@ -123,30 +129,42 @@ int paging_map_page(void *physical_address, void *virtual_address,
 
 void paging_unmap_page(void *virtual_address)
 {
+  klog(LOG_DEBUG, "paging_unmap_page: unmapping 0x%x\n", virtual_address);
+
   struct page_table_entry *page_table;
   uintptr_t page_dir_index = (uintptr_t)virtual_address >> 22;
   uintptr_t page_tab_index =
     (uintptr_t)virtual_address >> 12 & 0x3FF; // TODO(coditva): magic num
 
+  assert(page_directory[page_dir_index].present);
   page_table = (struct page_table_entry *)
       page_directory_to_address(&page_directory[page_dir_index]);
   assert(page_table[page_tab_index].present);
+
   page_table_set_value(&page_table[page_tab_index], 0x0);
 }
 
 #ifdef DEBUG
 void paging_dump_map(void)
 {
+  klog(LOG_DEBUG, LOG_HRULE);
+  klog(LOG_DEBUG, "PAGING MEMORY DUMP START\n");
+  klog(LOG_DEBUG, LOG_HRULE);
+
   for (uint32_t i = 0; i < PAGE_DIR_SIZE; i++) {
-    uint32_t *page_table = page_directory_to_address(&page_directory[i]);
-    if (((uintptr_t)page_table & 0x00000001) == 0) {
+    struct page_directory_entry *page_dir = &page_directory[i];
+    if (!page_dir->present) {
       continue;
     }
-    page_table = (void *)((uintptr_t)page_table & 0xfffff000);
-    klog(LOG_DEBUG, "page page at 0x%x\n", page_table);
+    struct page_table_entry *page_table = page_directory_to_address(page_dir);
+    klog(LOG_DEBUG, "page dir at 0x%x\n", page_table);
     for (uint32_t j = 0; j < PAGE_TAB_SIZE; j++) {
-      klog(LOG_DEBUG, "-- page table entry: 0x%x\n", page_table[j]);
+      klog(LOG_DEBUG, "- page table entry: 0x%x\n", page_table[j]);
     }
   }
+
+  klog(LOG_DEBUG, LOG_HRULE);
+  klog(LOG_DEBUG, "PAGING MEMORY DUMP END\n");
+  klog(LOG_DEBUG, LOG_HRULE);
 }
 #endif
