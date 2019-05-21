f72c7455de8c18d450d4eaa29d6b90e700027857
Utkarsh Maheshwari <coditva@gmail.com>
2019-05-21 14:49:42 +0530

    mm: Implement page_directory, page_table functions

--- kernel/mm/page_directory.c
+++ kernel/mm/page_directory.c
@@ -1,15 +1,26 @@
-#include <helper.h>
 #include <mm/page_directory.h>
+#include <mm/page_frame.h>
+#include <mm/paging.h>
 #include <types.h>
 
-void * page_directory_get(uintptr_t index)
+inline void * page_directory_get(uintptr_t index)
 {
-  MARK_PARAM_USED(index);
+  if (page_directory[index] & 0x00000001) {
+    return (void *)(page_directory[index] & 0xfffff000);
+  }
   return NULL;
 }
 
-void * page_directory_add(uintptr_t index)
+inline void * page_directory_add(uintptr_t index)
 {
-  MARK_PARAM_USED(index);
-  return NULL;
+  if (page_directory[index] & 0x00000001) {
+    return NULL;
+  }
+
+  uint32_t *page_table_entry = page_frame_alloc();
+  for (uint32_t i = 0; i < PAGE_TAB_SIZE; i++) {
+    page_table_entry[i] = 0x00000000;
+  }
+  page_directory[index] = (uint32_t)((uintptr_t)page_table_entry) | 3;
+  return (void *)(page_directory[index] & 0xfffff000);
 }
--- kernel/mm/page_table.c
+++ kernel/mm/page_table.c
@@ -1,26 +1,32 @@
-#include <helper.h>
+#include <mm/page_directory.h>
 #include <mm/page_table.h>
 #include <types.h>
 
-void * page_table_get(uintptr_t page_dir_index, uintptr_t index)
+inline void * page_table_get(uintptr_t page_dir_index, uintptr_t index)
 {
-  MARK_PARAM_USED(page_dir_index);
-  MARK_PARAM_USED(index);
-  return NULL;
+  uint32_t *page_dir = page_directory_get(page_dir_index);
+  if (page_dir[index] & 0x00000000) { /* entry not present */
+    return NULL;
+  }
+
+  return (void *)((uintptr_t)&page_dir[index] & 0xfffff000);
 }
 
-void * page_table_add(uintptr_t page_dir_index, uintptr_t index,
+inline void * page_table_add(uintptr_t page_dir_index, uintptr_t index,
     void *physical_address, uint32_t flags)
 {
-  MARK_PARAM_USED(index);
-  MARK_PARAM_USED(page_dir_index);
-  MARK_PARAM_USED(physical_address);
-  MARK_PARAM_USED(flags);
-  return NULL;
+  uint32_t *page_dir = page_directory_get(page_dir_index);
+
+  if (page_dir[index] & 0x00000001) { /* entry already present */
+    return NULL;
+  }
+
+  page_dir[index] = ((uintptr_t)physical_address | (flags & 0xfff) | 0x01);
+  return (void *)((uintptr_t)&page_dir[index] & 0xfffff000);
 }
 
-void page_table_remove(uintptr_t page_dir_index, uintptr_t index)
+inline void page_table_remove(uintptr_t page_dir_index, uintptr_t index)
 {
-  MARK_PARAM_USED(page_dir_index);
-  MARK_PARAM_USED(index);
+  uint32_t *page_dir = page_directory_get(page_dir_index);
+  page_dir[index] = 0x0;
 }
