5e4d1f9123036b22869e1b7af8974cb5db7aad5f
Utkarsh Maheshwari <coditva@gmail.com>
2019-05-22 14:29:48 +0530

    test: Define kcheck when debugging (defined DEBUG)

--- kernel/include/kcheck.h
+++ kernel/include/kcheck.h
@@ -1,6 +1,10 @@
 #ifndef KCHECK_H_XRYFQPCA
 #define KCHECK_H_XRYFQPCA
 
+#include <config.h>
+
+# ifdef DEBUG
+
 /**
  * Check if the condition is true and print if check passed on LOG_INFO and if
  * failed on LOG_ERROR
@@ -9,5 +13,9 @@
  * @param char *    the label to describe the check
  */
 void kcheck(int condition, char *label);
+
+# else
+#  define kcheck(...)
+# endif
 
 #endif /* end of include guard: KCHECK_H_XRYFQPCA */
--- kernel/lib/kcheck.c
+++ kernel/lib/kcheck.c
@@ -1,14 +1,13 @@
-#include <helper.h>
 #include <kcheck.h>
 #include <logger.h>
 
+#ifdef DEBUG
 void kcheck(int condition, char *label)
 {
-  MARK_PARAM_USED(label);
-
   if (condition) {
     klog_status_ok(label);
   } else {
     klog_status_fail(label);
   }
 }
+#endif
