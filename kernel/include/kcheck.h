12ec9a0f54b13953f0b3277ae816db274b7e544d
Utkarsh Maheshwari <coditva@gmail.com>
2019-05-22 14:59:53 +0530

    docs: Add usage notes for assert, kcheck

--- kernel/include/kcheck.h
+++ kernel/include/kcheck.h
@@ -7,7 +7,7 @@
 
 /**
  * Check if the condition is true and print if check passed on LOG_INFO and if
- * failed on LOG_ERROR
+ * failed on LOG_ERROR. Use for sanity checks and such.
  *
  * @param int       the expression to check
  * @param char *    the label to describe the check
