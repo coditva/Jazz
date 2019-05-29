5fd9ceefcb951ed6e70cf630b1f9bfb12518f681
Utkarsh Maheshwari <coditva@gmail.com>
2019-05-29 13:33:06 +0530

    docs: Add documentation for page_alloc

--- kernel/include/mm/page_alloc.h
+++ kernel/include/mm/page_alloc.h
@@ -4,10 +4,39 @@
 #include <mm/types.h>
 #include <types.h>
 
-void            page_alloc_init();
-struct page *   page_alloc();
-struct page *   page_n_alloc(size_t);
-void            page_free(struct page *);
-void            page_n_free(struct page *, size_t);
+/**
+ * Initialize page_alloc.
+ */
+void            page_alloc_init     ();
+
+/**
+ * Allocate a physical page from memory.
+ *
+ * @return  struct page *   Allocated page
+ */
+struct page *   page_alloc          ();
+
+/**
+ * Allocate n number of contiguous pages from memory.
+ *
+ * @param   size_t          Number of pages to allocate
+ * @return  struct page *   First allocated page
+ */
+struct page *   page_n_alloc        (size_t);
+
+/**
+ * Free an allocated page in memory.
+ *
+ * @param   struct page *   The page to free
+ */
+void            page_free           (struct page *);
+
+/**
+ * Free n allocated contiguous pages in memory.
+ *
+ * @param   struct page *   First page in n contiguous pages
+ * @return  size_t          Number of pages to free
+ */
+void            page_n_free         (struct page *, size_t);
 
 #endif /* end of include guard: PAGE_ALLOC_C_YUK9OBSD */
