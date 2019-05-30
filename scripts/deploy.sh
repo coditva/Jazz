47eca33e853d63d357554a8006e3e7af84775545
Utkarsh Maheshwari <coditva@gmail.com>
2019-05-30 19:50:45 +0530

    fixup! build: Rename disk images before deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ if [[ -z "$ARCH" ]]; then
     ARCH=$DEFAULT_ARCH
 fi
 
-RELEASE_FILE_NAME="Jazz_$(ARCH)_$(RELEASE_TYPE).img"
+RELEASE_FILE_NAME="Jazz_$ARCH_$RELEASE_TYPE.img"
 mv build/disk.img $RELEASE_FILE_NAME
 
 export $RELEASE_FILE_NAME
