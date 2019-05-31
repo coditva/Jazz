c83fb1d31591c1bb7504bc948c81c3a441590826
Utkarsh Maheshwari <coditva@gmail.com>
2019-05-31 22:01:08 +0530

    types: Remove obsolete code after setting up cross compiler

--- kernel/arch/x86/include/types.h
+++ kernel/arch/x86/include/types.h
@@ -3,9 +3,6 @@
 
 #include "arch/generic/include/types.h"
 
-/* TODO: Setup compiler toolchain and remove this */
-#if 0
-
 #ifdef CONFIG_64BIT
 # define BITS_PER_LONG 64
 # define __kernel_ulong_t    __u64
@@ -14,14 +11,6 @@
 # define BITS_PER_LONG 32
 # define __kernel_ulong_t    __u32
 # define __kernel_long_t     __s32
-#endif
-
-#else
-
-# define BITS_PER_LONG 32
-# define __kernel_ulong_t    unsigned long
-# define __kernel_long_t     unsigned long
-
 #endif
 
 #endif /* end of include guard: TYPES_H_6BL3PEFC */
